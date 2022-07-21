24c8eec4510419abf96c6cf59b850c3a51a2bdcb
Pedro Knok <pedrokn9@gmail.com>
2022-07-21 02:09:18 -0300

    FEAT: Desenvolvimento Home - Carousel/colors

--- MinhaChanceApp/src/app/home-component/home-component.component.ts
+++ MinhaChanceApp/src/app/home-component/home-component.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponentComponent implements OnInit {
 
+
+  images = [
+  'https://st.depositphotos.com/2461781/3126/v/600/depositphotos_31260521-stock-illustration-hand-shaking.jpg',
+  'https://static.vecteezy.com/ti/vetor-gratis/p1/5318194-recrutamento-conceito-ilustracao-ideia-para-pagina-de-destino-modelo-membro-selecao-contratacao-profissional-com-habilidades-especificas-para-trabalho-planejamento-de-negocios-estilos-planos-desenhados-a-mao-vetor.jpg',
+  'https://img.freepik.com/vetores-premium/feedback-ou-conceito-de-ilustracao-de-avaliacao-com-personagens-pessoas-felizes-segurando-cinco-estrelas-sobre-suas-cabecas_269730-104.jpg',
+  'https://congresse.me/wp-content/uploads/2022/05/Curso-online.png',
+  'https://thumbs.dreamstime.com/b/desenvolvimento-profissional-de-professores-%E2%80%94-ilustra%C3%A7%C3%A3o-do-vetor-conceito-abstrato-forma%C3%A7%C3%A3o-por-iniciativa-das-autoridades-189702706.jpg',
+  'https://st2.depositphotos.com/7523024/43987/v/450/depositphotos_439871530-stock-illustration-certificate-vector-icon-modern-simple.jpg',
+  'https://classic.exame.com/wp-content/uploads/2021/06/GettyImages-1041190304.jpg?quality=70&strip=info&w=1024',].map((n) => `${n}`);
+
+  imagesActors = [
+    'https://thumbs.dreamstime.com/b/carta-de-identifica%C3%A7%C3%A3o-licen%C3%A7a-como-conceito-estrutura-t%C3%B3picos-documento-identidade-v%C3%A1lido-condu%C3%A7%C3%A3o-ou-permiss%C3%A3o-viagem-201788862.jpg',
+    'https://thumbs.dreamstime.com/b/grupo-criativo-linha-moderna-ilustra%C3%A7%C3%A3o-do-estilo-projeto-133672888.jpg',
+    ].map((n) => `${n}`);
+
   constructor() { }
 
   ngOnInit(): void {
