8c05d6e0a3060732fe3634976570330c10b2b508
Pedro Knok <pedrokn9@gmail.com>
2022-09-03 03:55:13 -0300

    WIP: Grid perguntas navegaçao teste

--- MinhaChanceApp/src/app/skills-test-component/skills-test/skills-test.component.ts
+++ MinhaChanceApp/src/app/skills-test-component/skills-test/skills-test.component.ts
@@ -1,4 +1,7 @@
+import { DataSource } from '@angular/cdk/collections';
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+
 
 @Component({
   selector: 'app-skills-test',
@@ -7,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillsTestComponent implements OnInit {
 
+    data: any[] = [
+    {firstRow: 1, secondRow: 2, thirdRow: 3, forthRow: 4, fifthRow: 5},
+    {firstRow: 6, secondRow: 7, thirdRow: 8, forthRow: 9, fifthRow: 10},
+    {firstRow: 11, secondRow: 12, thirdRow: 13, forthRow: 14, fifthRow: 15},
+    {firstRow: 16, secondRow: 17, thirdRow: 18, forthRow: 19, fifthRow: 20},
+  ];
+  
+
+  displayedColumns: string[] = ['firstRow', 'secondRow', 'thirdRow', 'forthRow', 'fifthRow'];
+  dataSource = new MatTableDataSource(this.data);
 
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
